efaf53646c344cd52e16da7fff43b5a4fee3ebbf
Agerr <agerrrr@proton.me>
2022-10-05 18:40:39 +0300

    a

---
+++ .startup.sh
@@ -0,0 +1,36 @@
+#!/bin/shell
+
+pkill alacritty
+pkill spotify
+pkill Discord
+
+commands=(
+  'alacritty -e btop'
+  'alacritty -e cava'
+  'alacritty'
+  'discord'
+  'spotify'
+  'pkill .startup'
+)
+
+pids=""
+
+for command in "${commands[@]}"; do
+  sleep 0.1
+  echo command is ${command}
+  ( ${command} ) &
+  pid=$!
+  pids+=" ${pid}"
+  echo "${pid}: Running: ${command}"
+done
+
+for p in $pids; do
+  if wait $p; then
+    echo "Process $p succeeded."
+  else
+    echo "Process $p failed. Build aborted."
+    echo "Killing any running concurrent commands."
+    kill $pids &> /dev/null
+    exit 1
+  fi
+done
