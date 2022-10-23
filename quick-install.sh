a33135aca34b59bcaadfc4f2abf57c3b88a2223b
Agerr <agerrrr@proton.me>
2022-10-23 17:32:07 +0300

    + calendar

--- quick-install.sh
+++ quick-install.sh
@@ -1,6 +1,6 @@
 #!/bin/shell
 sudo pacman -S xorg mesa lib32-mesa xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon libva-mesa-driver lib32-libva-mesa-driver alsa-utils pulseaudio pulseaudio-alsa
-sudo pacman -S zsh zsh-completions lightdm lightdm-slick-greeter i3 alacritty dmenu rofi xdg-user-dirs xss-lock polkit-gnome gnome-keyring feh mplayer picom firefox thunar gparted deepin-calculator pavucontrol lxappearance qt5ct gdb neofetch btop tldr tree unzip wget flameshot gpick dunst ttf-jetbrains-mono ttf-dejavu ttf-droid noto-fonts noto-fonts-emoji gvfs ntfs-3g
+sudo pacman -S zsh zsh-completions lightdm lightdm-slick-greeter i3 alacritty dmenu rofi xdg-user-dirs xss-lock polkit-gnome gnome-keyring feh mplayer picom firefox thunar gparted deepin-calculator deepin-calendar pavucontrol lxappearance qt5ct gdb neofetch btop tldr tree unzip wget flameshot gpick dunst ttf-jetbrains-mono ttf-dejavu ttf-droid noto-fonts noto-fonts-emoji gvfs ntfs-3g
 yay -S systemd-numlockontty numlockx i3lock-fancy-git cava autotiling nerd-fonts-jetbrains-mono visual-studio-code-bin icu69 gitkraken spotify timeshift discord-ptb betterdiscord-installer grub-customizer spicetify-cli onlyoffice-bin
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 mkdir -p ~/Source/git/
