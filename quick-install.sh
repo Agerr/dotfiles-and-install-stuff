5f1a28e88dd724cfe1aadac46b856ab523a0849f
Agerr <agerrrr@proton.me>
2022-10-31 22:34:51 +0200

    name issue fixed

--- quick-install.sh
+++ quick-install.sh
@@ -13,27 +13,27 @@ sudo systemctl enable cronie.service
 xdg-user-dirs-update
 echo "QT_QPA_PLATFORMTHEME=qt5ct" | sudo tee --append /etc/environment
 mkdir -p ~/.config/i3
-ln -sf ~/Source/git/dotfiles/.config/i3/config ~/.config/i3/config
+ln -sf ~/Source/git/dotfiles-and-install-stuff/.config/i3/config ~/.config/i3/config
 mkdir ~/.config/i3status
-ln -sf ~/Source/git/dotfiles/.config/i3status/config ~/.config/i3status/config
-ln -sf ~/Source/git/dotfiles/.Xresources ~/.Xresources
-ln -s ~/Source/git/dotfiles/Wallpapers/dracula_arch.png ~/Pictures/background.png
-sudo cp ~/Source/git/dotfiles/20-amdgpu.conf /etc/X11/xorg.conf.d/20-amdgpu.conf
+ln -sf ~/Source/git/dotfiles-and-install-stuff/.config/i3status/config ~/.config/i3status/config
+ln -sf ~/Source/git/dotfiles-and-install-stuff/.Xresources ~/.Xresources
+ln -s ~/Source/git/dotfiles-and-install-stuff/Wallpapers/dracula_arch.png ~/Pictures/background.png
+sudo cp ~/Source/git/dotfiles-and-install-stuff/20-amdgpu.conf /etc/X11/xorg.conf.d/20-amdgpu.conf
 sudo mkdir -p /usr/share/backgrounds
-sudo cp ~/Source/git/dotfiles/Wallpapers/dracula_base.png /usr/share/backgrounds/background.png
-sudo cp ~/Source/git/dotfiles/picom.conf /etc/xdg/picom.conf
-sudo cp ~/Source/git/dotfiles/lightdm.conf /etc/lightdm/lightdm.conf
-sudo cp ~/Source/git/dotfiles/slick-greeter.conf /etc/lightdm/slick-greeter.conf
-ln -sf ~/Source/git/dotfiles/.config/alacritty ~/.config/alacritty
-ln -sf ~/Source/git/dotfiles/.zshrc ~/.zshrc
-cp ~/Source/git/dotfiles/Dracula-zsh/dracula.zsh-theme ~/.oh-my-zsh/themes/
-cp -r ~/Source/git/dotfiles/Dracula-zsh/lib/ ~/.oh-my-zsh/themes/
-ln -sf ~/Source/git/dotfiles/.config/dunst ~/.config/dunst
-ln -sf ~/Source/git/dotfiles/.config/cava ~/.config/cava
+sudo cp ~/Source/git/dotfiles-and-install-stuff/Wallpapers/dracula_base.png /usr/share/backgrounds/background.png
+sudo cp ~/Source/git/dotfiles-and-install-stuff/picom.conf /etc/xdg/picom.conf
+sudo cp ~/Source/git/dotfiles-and-install-stuff/lightdm.conf /etc/lightdm/lightdm.conf
+sudo cp ~/Source/git/dotfiles-and-install-stuff/slick-greeter.conf /etc/lightdm/slick-greeter.conf
+ln -sf ~/Source/git/dotfiles-and-install-stuff/.config/alacritty ~/.config/alacritty
+ln -sf ~/Source/git/dotfiles-and-install-stuff/.zshrc ~/.zshrc
+cp ~/Source/git/dotfiles-and-install-stuff/Dracula-zsh/dracula.zsh-theme ~/.oh-my-zsh/themes/
+cp -r ~/Source/git/dotfiles-and-install-stuff/Dracula-zsh/lib/ ~/.oh-my-zsh/themes/
+ln -sf ~/Source/git/dotfiles-and-install-stuff/.config/dunst ~/.config/dunst
+ln -sf ~/Source/git/dotfiles-and-install-stuff/.config/cava ~/.config/cava
 mkdir -p ~/.config/BetterDiscord/themes
-ln -sf ~/Source/git/dotfiles/.config/BetterDiscord/themes/dracula.theme.css ~/.config/BetterDiscord/themes/dracula.theme.css
-sudo cp -r ~/Source/git/dotfiles/Dracula-GTK /usr/share/themes/Dracula
-sudo cp -r ~/Source/git/dotfiles/Dracula-icons /usr/share/icons/
+ln -sf ~/Source/git/dotfiles-and-install-stuff/.config/BetterDiscord/themes/dracula.theme.css ~/.config/BetterDiscord/themes/dracula.theme.css
+sudo cp -r ~/Source/git/dotfiles-and-install-stuff/Dracula-GTK /usr/share/themes/Dracula
+sudo cp -r ~/Source/git/dotfiles-and-install-stuff/Dracula-icons /usr/share/icons/
 mkdir -p ~/.config/qt5ct/colors/
-cp ~/Source/git/dotfiles/Dracula-qt5/Dracula.conf ~/.config/qt5ct/colors/
-ln -sf ~/Source/git/dotfiles/.startup.sh ~/.startup.sh
+cp ~/Source/git/dotfiles-and-install-stuff/Dracula-qt5/Dracula.conf ~/.config/qt5ct/colors/
+ln -sf ~/Source/git/dotfiles-and-install-stuff/.startup.sh ~/.startup.sh
