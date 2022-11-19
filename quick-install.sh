30924a030296da73848e4bc23cad445a16b28658
Agerr <78555082+Agerr@users.noreply.github.com>
2022-11-19 21:45:00 +0200

    Update quick-install.sh

--- quick-install.sh
+++ quick-install.sh
@@ -1,7 +1,7 @@
 #!/bin/shell
 sudo pacman -S xorg mesa lib32-mesa xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon libva-mesa-driver lib32-libva-mesa-driver alsa-utils pulseaudio pulseaudio-alsa
 sudo pacman -S zsh zsh-completions lightdm lightdm-slick-greeter i3 alacritty dmenu rofi xdg-user-dirs xss-lock polkit-gnome gnome-keyring feh mplayer picom firefox thunar gparted deepin-calculator deepin-calendar pavucontrol lxappearance qt5ct gdb neofetch btop tldr tree unzip wget flameshot gpick dunst ttf-jetbrains-mono ttf-dejavu ttf-droid noto-fonts noto-fonts-emoji gvfs ntfs-3g
-paru -S --noconfirm systemd-numlockontty numlockx i3lock-fancy-git cava autotiling nerd-fonts-jetbrains-mono visual-studio-code-bin icu69-bin gitkraken spotify timeshift discord-ptb betterdiscord-installer grub-customizer spicetify-cli onlyoffice-bin
+paru -S --noconfirm systemd-numlockontty numlockx i3lock-fancy-git cava autotiling nerd-fonts-jetbrains-mono visual-studio-code-bin icu69-bin gitkraken spotify timeshift discord-ptb betterdiscord-installer-bin grub-customizer spicetify-cli onlyoffice-bin
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 mkdir -p ~/Source/git/
 cd ~/Source/git/ && git clone --depth=1 https://github.com/adi1090x/rofi.git
@@ -30,7 +30,7 @@ cp ~/Source/git/dotfiles-and-install-stuff/Dracula-zsh/dracula.zsh-theme ~/.oh-m
 cp -r ~/Source/git/dotfiles-and-install-stuff/Dracula-zsh/lib/ ~/.oh-my-zsh/themes/
 ln -sf ~/Source/git/dotfiles-and-install-stuff/.config/dunst ~/.config/dunst
 ln -sf ~/Source/git/dotfiles-and-install-stuff/.config/cava ~/.config/cava
-mkdir -p ~/.config/BetterDiscord/themes
+mkdir -p ~/.config/rord/themes
 ln -sf ~/Source/git/dotfiles-and-install-stuff/.config/BetterDiscord/themes/dracula.theme.css ~/.config/BetterDiscord/themes/dracula.theme.css
 sudo cp -r ~/Source/git/dotfiles-and-install-stuff/Dracula-GTK /usr/share/themes/Dracula
 sudo cp -r ~/Source/git/dotfiles-and-install-stuff/Dracula-icons /usr/share/icons/
