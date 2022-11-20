ea097cd2dc331101e0569aef40e12cea2d7eef96
agerr <agerrrr@poron.me>
2022-11-20 12:06:04 +0200

    a

--- quick-install.sh
+++ quick-install.sh
@@ -1,7 +1,7 @@
 #!/bin/shell
 sudo pacman -S xorg mesa lib32-mesa xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon libva-mesa-driver lib32-libva-mesa-driver alsa-utils pulseaudio pulseaudio-alsa
 sudo pacman -S zsh zsh-completions lightdm lightdm-slick-greeter i3 alacritty dmenu rofi xdg-user-dirs xss-lock polkit-gnome gnome-keyring feh mplayer picom firefox thunar gparted deepin-calculator deepin-calendar pavucontrol lxappearance qt5ct gdb neofetch btop tldr tree unzip wget flameshot gpick dunst ttf-jetbrains-mono ttf-dejavu ttf-droid noto-fonts noto-fonts-emoji gvfs ntfs-3g
-paru -S --noconfirm systemd-numlockontty numlockx i3lock-fancy-git cava autotiling nerd-fonts-jetbrains-mono visual-studio-code-bin icu69-bin gitkraken spotify-dev timeshift discord-ptb betterdiscord-installer-bin grub-customizer spicetify-cli onlyoffice-bin
+paru -S --noconfirm systemd-numlockontty numlockx i3lock-fancy-git cava autotiling nerd-fonts-jetbrains-mono visual-studio-code-bin icu69-bin gitkraken spotify-edge timeshift discord-ptb betterdiscord-installer-bin grub-customizer spicetify-cli onlyoffice-bin
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 mkdir -p ~/Source/git/
 cd ~/Source/git/ && git clone --depth=1 https://github.com/adi1090x/rofi.git
